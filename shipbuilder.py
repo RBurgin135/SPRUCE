9556b9fb75476bb34e95425be757ed7ed907395f
RBurgin <lonelymanwillspamyou@gmail.com>
2020-05-13 14:34:21 +0100

    sounds are in and small additions to left bar, and flight transition added

--- shipbuilder.py
+++ shipbuilder.py
@@ -26,7 +26,7 @@ class Board:
         self.RUN = True
         self.parts = []
         self.particles = []
-        self.credits = 1000
+        self.credits = 900
 
         #sounds
         self.ambience = pygame.mixer.Sound("sounds\\ambience.wav")
@@ -38,62 +38,61 @@ class Board:
         self.coord = [scr_width//2-(self.width*self.thickness//2), scr_height//2-(self.height*self.thickness//2)]
 
         #left bar
-        self.BarWidth = 300
-        self.items = [Hull(False), Cockpit(False), Engine(False), Gun(False)]
-        self.showcase = []
-        self.gap = 230
+        self.BarWidth = 260
+        self.items = [Cockpit(False), Engine(False), Gun(False), Block(False), Corner(False), Concave(False)]
+        self.gap = 920 // len(self.items)
+        self.BuildmodeTransition()
         
-        #items
-        for i in self.items:
-            self.parts.append(i)
-            if len(self.showcase) < 1:
-                x, y = 0, 100
-            else:
-                x, y = self.showcase[-1][0], self.showcase[-1][1] + self.gap
-            i.coord = [x+self.BarWidth//2 , y+self.gap//2]
-            self.showcase.append([x , y])
 
     
     def Show(self):
         #board
-        for x in range(0,self.width):
-            for y in range(0, self.height):
-                #outer
-                pygame.draw.rect(window, (40,62,63), (self.coord[0]+(self.thickness*x), self.coord[1]+(self.thickness*y), self.thickness, self.thickness))
+        if self.buildmode:
+            for x in range(0,self.width):
+                for y in range(0, self.height):
+                    #outer
+                    pygame.draw.rect(window, (40,62,63), (self.coord[0]+(self.thickness*x), self.coord[1]+(self.thickness*y), self.thickness, self.thickness))
 
-                #inner
-                details = [self.coord[0]+(self.thickness*x)+1, self.coord[1]+(self.thickness*y)+1, self.thickness-2, self.thickness-2]
-                if x == 0:
-                    details[0] -= 1
-                    details[2] += 1
-                elif x == self.width-1:
-                    details[0] += 1
-                    details[2] += 1
+                    #inner
+                    details = [self.coord[0]+(self.thickness*x)+1, self.coord[1]+(self.thickness*y)+1, self.thickness-2, self.thickness-2]
+                    if x == 0:
+                        details[0] -= 1
+                        details[2] += 1
+                    elif x == self.width-1:
+                        details[0] += 1
+                        details[2] += 1
 
-                if y == 0:
-                    details[1] -= 1
-                    details[3] += 1
-                elif y == self.height-1:
-                    details[1] += 1
-                    details[3] += 1 
+                    if y == 0:
+                        details[1] -= 1
+                        details[3] += 1
+                    elif y == self.height-1:
+                        details[1] += 1
+                        details[3] += 1 
 
-                pygame.draw.rect(window, (33,44,48), details)
-        
-        #leftbar=======
-        pygame.draw.rect(window, (40,62,63), (0, 0, self.BarWidth, scr_height))
-        #highlights
-        M.highlight = -1
-        for i in self.showcase:
-            if (i[0] < M.coord[0] < i[0] + self.BarWidth) and (i[1] < M.coord[1] < i[1] + self.gap):
-                M.highlight = self.showcase.index(i)
-                pygame.draw.rect(window, (60,82,83), (i[0], i[1], self.BarWidth, self.gap))
-        
+                    pygame.draw.rect(window, (33,44,48), details)
+            
+            #leftbar=======
+            pygame.draw.rect(window, (40,62,63), (0, 0, self.BarWidth, scr_height))
+            #highlights
+            M.highlight = -1
+            for i in self.showcase:
+                if (i[0] < M.coord[0] < i[0] + self.BarWidth) and (i[1] < M.coord[1] < i[1] + self.gap):
+                    M.highlight = self.showcase.index(i)
+                    pygame.draw.rect(window, (60,82,83), (i[0], i[1], self.BarWidth, self.gap))
 
-        #Credits
-        SubFont = pygame.font.SysFont('', 100)
-        Text = SubFont.render("CR "+str(self.credits), False, (247,216,148))
-        #pygame.draw.rect(window, (60,82,83), (0, 0, self.BarWidth, 100))
-        window.blit(Text,(10,10))
+                    #prices
+                    item = self.items[M.highlight]
+                    PrcFont = pygame.font.SysFont('', 25)
+                    Text = PrcFont.render(str(item.cost), False, (247,216,148))
+                    window.blit(Text, (item.coord[0]-10, item.coord[1]+50))
+
+            
+
+            #Credits
+            SubFont = pygame.font.SysFont('', 100)
+            Text = SubFont.render("CR "+str(self.credits), False, (247,216,148))
+            #pygame.draw.rect(window, (60,82,83), (0, 0, self.BarWidth, 100))
+            window.blit(Text,(10,10))
 
         #signature
         SigFont = pygame.font.SysFont('', 25)
@@ -104,6 +103,29 @@ class Board:
         if not pygame.mixer.get_busy():
             pygame.mixer.Sound.play(self.ambience)
 
+    def FlightTransition(self):
+        self.buildmode = False
+
+        for i in self.items:
+            self.parts.remove(i)
+        
+    
+    def BuildmodeTransition(self):
+        self.buildmode = True
+
+        #left bar
+        self.showcase = []
+        #items
+        for i in self.items:
+            self.parts.append(i)
+            if len(self.showcase) < 1:
+                x, y = 0, 100
+            else:
+                x, y = self.showcase[-1][0], self.showcase[-1][1] + self.gap
+            i.coord = [x+self.BarWidth//2 , y+self.gap//2]
+            self.showcase.append([x , y])
+
+
 
 class Part:
     def __init__(self, held):
@@ -111,12 +133,18 @@ class Part:
         self.held = held
         self.pycoord = [False,False]
         self.coord = copy.deepcopy(M.coord)
+
+        #differentiate later===========
         self.deg = 0
         self.cost = 50
+        self.m = 10
 
         #image
         self.width = self.image.get_height()
         self.height = self.image.get_width()
+
+        #sound
+        self.liftsound = pygame.mixer.Sound("sounds\lift.wav")
 
     def Show(self):
         if self.held:
@@ -128,6 +156,9 @@ class Part:
         self.held = False
         self.pycoord = pycoord
         self.coord = [B.coord[0] + pycoord[0]*B.thickness+B.thickness//2, B.coord[1] + pycoord[1]*B.thickness+B.thickness//2]
+
+        #sound
+        pygame.mixer.Sound.play(random.choice(self.dropsound))
     
     def Lift(self):
         self.held = True
@@ -136,22 +167,54 @@ class Part:
 
 class Hull(Part):
     def __init__(self, held):
+        #sound
+        self.dropsound = []
+        for i in range(1,4):
+            self.dropsound.append(pygame.mixer.Sound("sounds\hull#"+str(i)+".wav"))
+
+        super().__init__(held)
+
+class Block(Hull):
+    def __init__(self, held):
         #image
         self.image = pygame.image.load("images\\1x1.png")
 
         super().__init__(held)
+
+class Corner(Hull):
+    def __init__(self, held):
+        #image
+        self.image = pygame.image.load("images\corner.png")
+
+        super().__init__(held)
+        self.cost = 25
+
+class Concave(Hull):
+    def __init__(self, held):
+        #image
+        self.image = pygame.image.load("images\concave.png")
+
+        super().__init__(held)
+        self.cost = 25
 
 class Cockpit(Part):
     def __init__(self, held):
         #image
         self.image = pygame.image.load("images\cockpit.png")
 
+        #sound
+        self.dropsound = [pygame.mixer.Sound("sounds\cockpit.wav")]
+
         super().__init__(held)
+        
 
 class Engine(Part):
     def __init__(self, held):
         #image
         self.image = pygame.image.load("images\engine.png")
+
+        #sound
+        self.dropsound = [pygame.mixer.Sound("sounds\engine.wav")]
 
         super().__init__(held)
 
@@ -160,7 +223,11 @@ class Gun(Part):
         #image
         self.image = pygame.image.load("images\gun.png")
 
+        #sound
+        self.dropsound = [pygame.mixer.Sound("sounds\gun.wav")]
+
         super().__init__(held)
+        
 
 #class Shield(Part):
 #    def __init__(self, held):
@@ -205,11 +272,12 @@ class Mouse:
 
     def Input(self):
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self.ClickDOWN()
-            if event.type == pygame.MOUSEBUTTONUP:
-                if self.selected!= False:
-                    self.ClickUP()
+            if B.buildmode:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.ClickDOWN()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if self.selected!= False:
+                        self.ClickUP()
             if event.type == pygame.MOUSEMOTION:
                 self.coord[0], self.coord[1] = pygame.mouse.get_pos()
             if event.type == pygame.KEYDOWN:
@@ -223,6 +291,11 @@ class Mouse:
                     if keys[pygame.K_RIGHT]:
                         self.selected.image = pygame.transform.rotate(self.selected.image, -90)
                         self.selected.deg -= 90
+                if keys[pygame.K_SPACE]:
+                    if B.buildmode:
+                        B.FlightTransition()
+                    else:
+                        B.BuildmodeTransition()
 
     def Coordfinder(self):
         #finds coords on board
@@ -264,23 +337,33 @@ class Mouse:
         elif B.credits > 0:
             #left bar
             if self.highlight == 0:
-                B.parts.append(Hull(True))
-                self.selected = B.parts[-1]
-            elif self.highlight == 1:
                 B.parts.append(Cockpit(True))
                 self.selected = B.parts[-1]
-            elif self.highlight == 2:
+            elif self.highlight == 1:
                 B.parts.append(Engine(True))
                 self.selected = B.parts[-1]
-            elif self.highlight == 3:
+            elif self.highlight == 2:
                 B.parts.append(Gun(True))
                 self.selected = B.parts[-1]
-
-            #credits
+            elif self.highlight == 3:
+                B.parts.append(Block(True))
+                self.selected = B.parts[-1]
+            elif self.highlight == 4:
+                B.parts.append(Corner(True))
+                self.selected = B.parts[-1]
+            elif self.highlight == 5:
+                B.parts.append(Concave(True))
+                self.selected = B.parts[-1]
+            
+            #credits + sound
             if self.selected != False:
+                #credits
                 B.credits -= self.selected.cost
                 if B.credits < 0:
                     B.credits = 0
+                
+                #sound
+                pygame.mixer.Sound.play(self.selected.liftsound)
             
 
     def ClickUP(self):
